2fb0cc0c0c5443637e2f243be023e149071c8e5d
John T. Wodder II <git@varonathe.org>
2018-10-12 22:40:54 +0000

    Update headerparser version

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+v1.1.0 (in development)
+-----------------------
+
+v1.0.0 (2018-10-12)
+-------------------
+Initial release
+
+This project's code was previously part of
+[Wheelodex](https://github.com/jwodder/wheelodex).
--- wheel_inspect/__init__.py
+++ wheel_inspect/__init__.py
@@ -17,7 +17,7 @@ Visit <https://github.com/jwodder/wheel-inspect> for more information.
 from .inspect import inspect_wheel
 from .schema  import SCHEMA
 
-__version__      = '1.0.0'
+__version__      = '1.1.0.dev1'
 __author__       = 'John Thorvald Wodder II'
 __author_email__ = 'wheel-inspect@varonathe.org'
 __license__      = 'MIT'
